bf91b429aab399577ecdf06353004e868d67c60f
IgorDerk <derk.i26252@gmail.com>
2025-02-14 02:51:40 +0100

    test hook

--- src/main/java/DemoWebShop/CreateAccountTests.java
+++ src/main/java/DemoWebShop/CreateAccountTests.java
@@ -83,5 +83,32 @@ public class CreateAccountTests extends TestBase {
         softAssert.assertAll();
     }
 
+    @Test
+    public void CreateAccountNegativeSoftAssertTest222222() {
+        SoftAssert softAssert = new SoftAssert();
+        driver.findElement(By.xpath("//a[.='Register']")).click();
+        driver.findElement(By.id("gender-male")).click();
+        driver.findElement(By.id("FirstName")).click();
+        driver.findElement(By.id("FirstName")).clear();
+        driver.findElement(By.id("FirstName")).sendKeys("Igor");
+        driver.findElement(By.id("LastName")).click();
+        driver.findElement(By.id("LastName")).clear();
+        driver.findElement(By.id("LastName")).sendKeys("Derk");
+        driver.findElement(By.id("Email")).click();
+        driver.findElement(By.id("Email")).clear();
+        driver.findElement(By.id("Email")).sendKeys("derk.i262523@gmail.com");
+        driver.findElement(By.id("Password")).click();
+        driver.findElement(By.id("Password")).clear();
+        driver.findElement(By.id("Password")).sendKeys("Password@1");
+        driver.findElement(By.id("ConfirmPassword")).click();
+        driver.findElement(By.id("ConfirmPassword")).clear();
+        driver.findElement(By.id("ConfirmPassword")).sendKeys("Password@1");
+        driver.findElement(By.id("register-button")).click();
+        softAssert.assertFalse(isElementPresent(By.xpath("//input[@value='Continue']"))); /* Проверяем,
+        что кнопка "Continue" НЕ появилась */
+        softAssert.assertTrue(isElementPresent(By.xpath("//li[.='The specified email already exists']")));
+        softAssert.assertAll();
+    }
+
 
 }
